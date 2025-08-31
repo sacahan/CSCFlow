acfcaa5aa9c44d39055bcb6e91e36b15614e2a5a
Brian Han <sacahan@gmail.com>
2025-08-31 18:57:01 +0800

    chore(build): 移除不必要的多平台支援設定

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -25,7 +25,8 @@ TARGET=$2
 MODE=$3
 
 # 預設要支援的平台
-PLATFORMS="linux/amd64,linux/arm64"
+# PLATFORMS="linux/amd64,linux/arm64"
+PLATFORMS="linux/amd64"
 # 若要推到 Docker Hub，預設的使用者帳號
 DOCKERHUB_USER="sacahan"
 
